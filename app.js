31770e66a0a974da58da5890d06be4ef05313d64
Jose Collado <jcoll1020@gmail.com>
2019-10-26 14:40:39 -0400

    update

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const router = express.Router();
 const path = require('path');
 const data = require('./data');
 const projects = data.projects;
